7e716ea96f1feb1aa8fa671ec40ee297ef8f7648
Pierre Lasorak <pierre.lasorak@gmail.com>
2025-01-15 18:50:28 +0000

    now working

---
+++ client_over_ssh.py
@@ -0,0 +1,115 @@
+import click
+import click_shell
+from sh.contrib import ssh
+import sys
+import time
+import sh
+import logging
+import getpass
+import threading
+import os
+ssh_sessions = {}
+
+aggregated = ""
+command_buffer = ""
+ssh_logger = logging.getLogger('ssh_stdout')
+
+class SSHLauncherProcessWatcherThread(threading.Thread):
+    def __init__(self, host, process):
+        threading.Thread.__init__(self)
+        self.host = host
+        self.process = process
+
+    def run(self):
+        global ssh_sessions
+
+        try:
+            self.process.wait()
+
+        except sh.SignalException_SIGKILL as e:
+            pass
+
+        except sh.ErrorReturnCode as e:
+            print(e)
+
+        finally:
+            print(f'Host {self.host} process exited')
+            if self.host in ssh_sessions:
+                del ssh_sessions[self.host]
+
+
+def watch_process(host, process):
+    t = SSHLauncherProcessWatcherThread(host, process)
+    t.start()
+
+
+def ssh_interact(char, stdin):
+    global aggregated, command_buffer
+
+    ssh_logger.info(char)
+
+    if "drunc-local-process-launcher >" in char:
+        time.sleep(0.1)
+        return
+
+    if command_buffer == "":
+        time.sleep(0.1)
+        return
+
+    stdin.put(f'launch "{command_buffer}"\r')
+    command_buffer = ""
+
+
+@click_shell.shell(prompt='drunc-ssh-python > ')
+@click.option('--log-level', type=click.Choice(['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], case_sensitive=False), default='INFO')
+@click.pass_context
+def ssh_client(ctx, log_level:str):
+    logging.basicConfig(level=log_level.upper())
+    sh_logger = logging.getLogger('sh')
+    sh_logger.setLevel(logging.WARNING)
+
+    def cleanup():
+        global ssh_sessions
+        for session in ssh_sessions.values():
+            session.kill()
+        ssh_sessions = {}
+
+    ctx.call_on_close(cleanup)
+
+
+@ssh_client.command()
+@click.argument("cmd")
+@click.argument("host")
+def launch(cmd:str, host:str):
+    global ssh_sessions, command_buffer
+    pwd = os.getcwd()
+    username = getpass.getuser()
+    ssh_arguments = [
+        '-tt',
+        f'{username}@{host}',
+        f'cd {pwd} && source venv/bin/activate && python3 launcher.py',
+    ]
+
+    if host not in ssh_sessions:
+        try:
+            ssh_sessions[host] = sh.ssh(*ssh_arguments, _bg=True, _out=ssh_interact)
+            watch_process(host, ssh_sessions[host])
+        except Exception as e:
+            print(e)
+            return
+
+    print(f"Host {host} processes are children of {ssh_sessions[host].pid}")
+
+    command_buffer = cmd
+
+
+@ssh_client.command()
+def kill():
+    global ssh_sessions
+    for session in ssh_sessions.values():
+        session.kill()
+    ssh_sessions = {}
+
+
+if __name__ == '__main__':
+    ssh_client()
---
+++ launcher.py
@@ -0,0 +1,52 @@
+import signal
+import os
+import time
+import subprocess
+import click
+import click_shell
+
+def terminate_all(sig, frame):
+    print('SIGHUP received')
+    pgrp = os.getpgid(os.getpid())
+    os.killpg(pgrp, signal.SIGKILL)
+
+processes = []
+
+@click_shell.shell(prompt='drunc-local-process-launcher > ')
+def launcher():
+    global processes
+    signal.signal(signal.SIGHUP, terminate_all)
+
+@launcher.command()
+def echo():
+    print("echo")
+
+@launcher.command()
+@click.argument("cmd")
+def launch(cmd:str):
+    global processes
+    proc = subprocess.Popen(cmd.split(" "))
+    print(f"Started process with pid {proc.pid} ({cmd})")
+    processes.append(proc)
+
+
+@launcher.command()
+def ps():
+    global processes
+    for proc in processes:
+        print(f"Process {proc.pid}" if proc.poll() is None else f"Process {proc.pid} exited with code {proc.poll()}")
+
+
+@launcher.command()
+def killall():
+    global processes
+    for proc in processes:
+        if proc.poll() is not None:
+            continue
+        proc.kill()
+    processes = []
+
+
+if __name__ == '__main__':
+    print("Starting launcher")
+    launcher()
